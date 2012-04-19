0f3206c8cf1037caeb4df6f5532f808938c0eb5d
Jan Vesely <jano.vesely@gmail.com>
2012-04-19 16:31:54 -0400

    Error checking and multiple format support.

--- pa-simple.c
+++ pa-simple.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <sys/mman.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include "play.h"
 
@@ -9,12 +10,31 @@
 int play(int fd, size_t offset, size_t data_size, unsigned rate,
     unsigned sample_size, unsigned channels, bool sign)
 {
+
+	/* Only 8bit samles are supported in unsigned mode */
+	if (!sign && sample_size != 8)
+		return ENOTSUP;
+
 	pa_sample_spec ss = {
-		.format = PA_SAMPLE_S16NE,
+		.format = PA_SAMPLE_U8,
 		.channels = channels,
 		.rate = rate,
 	};
+	switch(sample_size)
+	{
+	case 8:  // 8bit samples must be unsigned
+		ss.format = PA_SAMPLE_U8; break;
+	case 16: //assume native endian
+		ss.format = PA_SAMPLE_S16NE; break;
+	case 24:
+		ss.format = PA_SAMPLE_S24NE; break;
+	case 32: //assume integer
+		ss.format = PA_SAMPLE_S32NE; break;
+	default:
+		return ENOTSUP;
+	}
 
+	int error = 0;
 
 	pa_simple *s =
 	    pa_simple_new(NULL,               // Use the default server.
@@ -25,16 +45,20 @@ int play(int fd, size_t offset, size_t data_size, unsigned rate,
 			  &ss,                // Our sample format.
 			  NULL,               // Use default channel map
 			  NULL,               // Use default buffering attributes.
-			  NULL               // Ignore error code.
+			  &error               // E error code.
 			  );
-	assert(s);
-	void *audio = mmap(NULL, data_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	assert(audio);
+	if (!s)
+		return error;
 
-	pa_simple_write(s, audio, data_size, NULL);
-	pa_simple_drain(s, NULL);
+	void *data = mmap(NULL, data_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (!data)
+		return ENOMEM;
 
-	munmap(audio, data_size);
+	error = 0;
+	if (!pa_simple_write(s, data, data_size, &error))
+		pa_simple_drain(s, &error);
+
+	munmap(data, data_size);
 	pa_simple_free(s);
-	return 0;
+	return error;
 }
