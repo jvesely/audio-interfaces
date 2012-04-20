eade87de3c2b419c8957eb1cd1977ba2dfc10daa
Jan Vesely <jano.vesely@gmail.com>
2012-04-19 23:25:48 -0400

    Do not mix pa and pa-simple.

--- pulse.c
+++ pulse.c
@@ -1,12 +1,7 @@
-
-#ifndef SIMPLE
 #include <pulse/context.h>
 #include <pulse/thread-mainloop.h>
 #include <pulse/stream.h>
 #include <pulse/operation.h>
-#else
-#include <pulse/simple.h>
-#endif
 
 #include <pulse/sample.h>
 #include <assert.h>
@@ -92,8 +87,6 @@ int play(int fd, size_t offset, size_t data_size, unsigned rate,
 		return ENOTSUP;
 	}
 
-#ifndef SIMPLE
-
 	/* This will do the work */
 	pa_threaded_mainloop *loop = pa_threaded_mainloop_new();
 	if (!loop)
@@ -114,7 +107,6 @@ int play(int fd, size_t offset, size_t data_size, unsigned rate,
 	printf("Created context.\n");
 
 	pa_context_set_state_callback(context, state_change, loop);
-
 	{
 		pa_threaded_mainloop_lock(loop);
 		pa_context_connect(context, NULL, PA_CONTEXT_NOAUTOSPAWN | PA_CONTEXT_NOFAIL, NULL);
@@ -142,7 +134,6 @@ int play(int fd, size_t offset, size_t data_size, unsigned rate,
 	printf("Created stream.\n");
 
 
-	int error = 0;
 	void *data = mmap(NULL, data_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (!data)
 		return ENOMEM;
@@ -195,34 +186,6 @@ int play(int fd, size_t offset, size_t data_size, unsigned rate,
 	pa_threaded_mainloop_stop(loop);
 	printf("Stopped main loop.\n");
 	pa_threaded_mainloop_free(loop);
-#else
 
-	pa_simple *s =
-	    pa_simple_new(NULL,               // Use the default server.
-			  "TEST APP",           // Our application's name.
-			  PA_STREAM_PLAYBACK,
-			  NULL,               // Use the default device.
-			  "Test",            // Description of our stream.
-			  &ss,                // Our sample format.
-			  NULL,               // Use default channel map
-			  NULL,               // Use default buffering attributes.
-			  &error               // E error code.
-			  );
-	if (!s)
-		return error;
-
-	void *data = mmap(NULL, data_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (!data)
-		return ENOMEM;
-
-	char nice[100];
-	printf("Playing: %s.\n", pa_sample_spec_snprint(nice, 100, &ss));
-	error = 0;
-	if (!pa_simple_write(s, data, data_size, &error))
-		pa_simple_drain(s, &error);
-
-	munmap(data, data_size);
-	pa_simple_free(s);
-#endif
-	return error;
+	return 0;
 }
