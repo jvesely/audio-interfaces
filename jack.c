2e97df54de050fcd4ab510ec59216d260d004980
Jan Vesely <jano.vesely@gmail.com>
2012-05-06 20:44:42 -0400

    jack: End playback when done playing.

--- jack.c
+++ jack.c
@@ -10,34 +10,44 @@
 typedef struct playback {
 	int fd;
 	jack_port_t *port;
+	volatile bool running;
 } playback_t ;
 
 int data_func(jack_nframes_t nframes, void *arg)
 {
 	playback_t *pb = arg;
 	void * buffer = jack_port_get_buffer(pb->port, nframes);
-	int ret = read(pb->fd, buffer, nframes * sizeof(jack_default_audio_sample_t));
-	return (ret > 0) ? 0 : ret;
+	const int ret =
+	    read(pb->fd, buffer, nframes * sizeof(jack_default_audio_sample_t));
+	if (ret <= 0)
+		pb->running = false;
+	return 0;;
 }
 
 
 int play(int fd, size_t offset, size_t data_size, unsigned rate,
     unsigned sample_size, unsigned channels, bool sign)
 {
-	jack_status_t status;
+	if (channels != 1 || sample_size != 32 || !sign)
+		return ENOTSUP;
+
 	jack_client_t *client =
-	    jack_client_open("simple player", JackNullOption, &status);
+	    jack_client_open("simple player", JackNullOption, NULL);
 	if (!client)
 		return ENOMEM;
-	printf("Opened jack client with status: %x.\n", status);
-	playback_t pb = {.fd = fd};
+	playback_t pb = {.fd = fd, .running = true};
 	pb.port = jack_port_register(client, "output", "32 bit float mono audio",
 	    JackPortIsOutput, 0);
 	if (pb.port) {
 		jack_set_process_callback(client, data_func, &pb);
 		jack_activate(client);
-		// wait here
-		sleep(10);
+		const char **ports =
+		    jack_get_ports(client, NULL, NULL,
+		        JackPortIsPhysical|JackPortIsInput);
+		jack_connect(client, jack_port_name(pb.port), ports[0]);
+		jack_connect(client, jack_port_name(pb.port), ports[1]);
+		while(pb.running)
+			sleep(1);
 	} else {
 		printf("Failed to open port.\n");
 	}
